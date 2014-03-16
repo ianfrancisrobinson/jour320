18b9d81c499dd35e5e53f61dc603c9af3ec10391
ianfrancisrobinson <ian.francis.robinson@gmail.com>
2014-03-16 15:08:43 -0500

    Moved to Dropbox

--- js/script.js
+++ js/script.js
@@ -1,36 +1,8 @@
-$("#fwd").click(advance);
-$('#back').click(rewind);
+alert('hi')
+$(document).ready(function() {
+    $('.One').click(function() {
+        $(this).toggleClass('Two');
+    });
 
-var starter=1;
-function addOne(){
-	if(starter>2){
-		//do nothing
-	}
-	else{
-		starter++; //add one to itself. Could do starter = starter + 1
-	}
-}
-
-var finisher=1;
-function subtractOne(){
-	if(finisher<4){
-		//do nothing
-	}
-	else{
-		finisher--; //add one to itself. Could do starter = starter + 1
-	}
-}
-
-function advance(){
-	$(".BarrierOnePhoto").attr("src","photo3.jpg");
-	$(".caption").html("MARS");
-	addOne();
-	$(".counter").html(starter);
-}
-
-function rewind(){
-	$(".BarrierOnePhoto").attr("src","photo2.jpg");
-	$(".caption").html("Title IX");
-	subtractOne();
-	$(".counter").html(finisher);
-}
+    
+});
