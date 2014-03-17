b762a8f7b6f0c80f7a95994d1e97c73217d21b90
ianfrancisrobinson <ian.francis.robinson@gmail.com>
2014-03-16 21:54:46 -0500

    Giant commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
+
+	$(".hide").hide();
+
     $('.One').click(function() {
         $(this).toggleClass('Two');
     });
 
+    $('.s1').waypoint(function() {
+  	$('#uva').fadeIn("slow");
+  	});
+
+  	$('.s2').waypoint(function() {
+  	$('#oku').fadeIn("slow");
+  	});
+
+  	$('.s3').waypoint(function() {
+  	$('#nd').fadeIn("slow");
+  	});
     
 });
