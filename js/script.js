a2ad245922c5a067c46c39b85138aa048baa0c47
ianfrancisrobinson <ian.francis.robinson@gmail.com>
2014-03-16 22:52:55 -0500

    Big push

--- js/script.js
+++ js/script.js
@@ -7,15 +7,124 @@ $(document).ready(function() {
     });
 
     $('.s1').waypoint(function() {
-  	$('#uva').fadeIn("slow");
+  		$('#uva').fadeIn("slow");
   	});
 
   	$('.s2').waypoint(function() {
-  	$('#oku').fadeIn("slow");
+  		$('#oku').fadeIn("slow");
   	});
 
   	$('.s3').waypoint(function() {
-  	$('#nd').fadeIn("slow");
+  		$('#nd').fadeIn("slow");
   	});
     
+    $('.s4').waypoint(function() {
+  		$('#dartmouth').fadeIn("slow");
+  	});
+
+  	$('.s5').waypoint(function() {
+  		$('#oxy').fadeIn("slow");
+  	});
+
+  	$('.s6').waypoint(function() {
+  		$('#unc').fadeIn("slow");
+  	});
+
+  	$('.s7').waypoint(function() {
+  		$('#usc').fadeIn("slow");
+  	});
+
+  	$('.s8').waypoint(function() {
+  		$('#boulder').fadeIn("slow");
+  	});
+
+  	$('.s9').waypoint(function() {
+  		$('#swart').fadeIn("slow");
+  	});
+
+  	$('.s10').waypoint(function() {
+  		$('#cedar').fadeIn("slow");
+  	});
+
+  	$('.s11').waypoint(function() {
+  		$('#amherst').fadeIn("slow");
+  	});
+
+  	$('.s12').waypoint(function() {
+  		$('#vandy').fadeIn("slow");
+  	});
+
+  	$('.s13').waypoint(function() {
+  		$('#ui').fadeIn("slow");
+  	});
+
+  	$('.s14').waypoint(function() {
+  		$('#cm').fadeIn("slow");
+  	});
+
+  	$('.s15').waypoint(function() {
+  		$('#penn').fadeIn("slow");
+  	});
+
+  	$('.s16').waypoint(function() {
+  		$('#hanover').fadeIn("slow");
+  	});
+
+  	$('.s17').waypoint(function() {
+  		$('#emerson').fadeIn("slow");
+  	});
+
+  	$('.s18').waypoint(function() {
+  		$('#uconn').fadeIn("slow");
+  	});
+
+  	$('.s19').waypoint(function() {
+  		$('#miss').fadeIn("slow");
+  	});
+
+  	$('.s20').waypoint(function() {
+  		$('#fsu').fadeIn("slow");
+  	});
+
+  	$('.s21').waypoint(function() {
+  		$('#cu').fadeIn("slow");
+  	});
+
+  	$('.s22').waypoint(function() {
+  		$('#berkely').fadeIn("slow");
+  	});
+
+  	$('.s23').waypoint(function() {
+  		$('#akron').fadeIn("slow");
+  	});
+
+  	$('.s24').waypoint(function() {
+  		$('#hobart').fadeIn("slow");
+  	});
+
+  	$('.s25').waypoint(function() {
+  		$('#uc').fadeIn("slow");
+  	});
+
+  	$('.s26').waypoint(function() {
+  		$('#msu').fadeIn("slow");
+  	});
+
+  	$('.s27').waypoint(function() {
+  		$('#nu').fadeIn("slow");
+  	});
+
+  	$('.s28').waypoint(function() {
+  		$('#smu').fadeIn("slow");
+  	});
+
+  	$('.s29').waypoint(function() {
+  		$('#bsu').fadeIn("slow");
+  	});
+
+  	$('.s30').waypoint(function() {
+  		$('#wesleyan').fadeIn("slow");
+  	});
+
+
 });
